2ed6a49e3e7390af2bd41a0f67077e840f36c96c
Vlad Debelak <vladdebelak@gmail.com>
2025-05-11 20:06:49 -0600

    Add comments

--- socialmedia.c
+++ socialmedia.c
@@ -66,6 +66,7 @@ typedef struct
     int num_posts;
     char followers[MAX_FOLLOWERS][50];
     int num_followers;
+    // I made this structure a linked list
     struct User *next;
 } User;
 // Function prototypes
@@ -73,7 +74,10 @@ void create_account(User **platform);
 void post_message(User **platform);
 void follow_user(User **platform);
 void generate_feed(User *platform);
+// I added these functions
+// This function loads whatever is in the file into the linked list
 User *loadPlatform(int *num_users);
+// This function saves whatever is in the linked list into the file
 void savePlatform(User *platform);
 /*
 void unfollow_user(User *users, int num_users);
@@ -83,57 +87,79 @@ void privacy_settings(User *users, int num_users);
 */
 int main()
 {
+    // this is just declaring the ints
     int num_users = 0, command = 0;
+    // This puts whatever is in the file into the platform User struct by using the load platform function
     User *platform = loadPlatform(&num_users);
-    
-    char users[MAX_USERS];
     // Test your functions here
+    // This loop is just for the command and what they want
+    // The time complexity is O(n) because it is just one while loop
     while (command != 5)
     {
+        // This just asks for what command the user wants
         printf("Create Account(1)\nPost Message(2)\nFollow User(3)\nGenerate Feed(4)\nSave and Quit(5)\n");
         printf("What command would you like?");
         scanf("%i", &command);
+        // This command is is to add a new user
         if (command == 1)
         {
+            // This is just to make sure there anr't too many users
             if (num_users < MAX_USERS)
             {
+                // Increase the amount of users and then create a new user with the function
                 num_users++;
                 create_account(&platform);
             }
             else
             {
+                // Just some error handling 
                 printf("Error: There is no more room for more users\n");
             }
         }
+        // For command 2 is posting a message
         else if (command == 2)
         {
+            // Access the function
             post_message(&platform);
         }
+        // Command command 3 is to follow a user
         else if (command == 3)
         {
+            // This is the function to follow the user
             follow_user(&platform);
         }
+        // This command 4 is to generate a feed
         else if (command == 4)
         {
-            // generate_feed(&platform,num_users);
+            //This function is to generate the feend
+            generate_feed(&platform);
         }
+        // This is if the user want to quit
         else if (command == 5)
         {
             break;
         }
+        // In case they input something else
         else
         {
             printf("Error: command not understood\n");
         }
     }
+    // This is to save it into the file
     savePlatform(platform);
     return 0;
 }
+// This function creates an account using the double pointer user struct platform
+// The time complexity of this function is O(n) because there is a loop
+// This function is void because we just add the informating at the end of the linked list and it is a pointer
 void create_account(User **platform)
 {
     // Implement creating a new user account
+    // This allocates memory to the new user struct
     User *newUser = malloc(sizeof(User));
+    // declaring chars
     char username[50], password[50], email[100];
+    // asking for the information and putting it into the linked list
     printf("What is your username?\n");
     scanf(" %49[^\n]", username);
     printf("What is your password?\n");
@@ -156,21 +182,30 @@ void create_account(User **platform)
     // Traverse till the last node
     while (last->next != NULL)
     {
+        // fo to the next one
         last = last->next;
     }
+    //Put it at the end of the linked list
     last->next = newUser;
 }
+// This function post a message
+// This functions time complexity is O(n) because it has one while loop
+// This function is void because we just put it into the matrix in the struct and we need the double pointer platform struct
 void post_message(User **platform)
 {
     // Implement posting a message;
+    // char username
     char username[50];
+    // ask for the useranem
     printf("What is your username\n");
     scanf(" %49[^\n]", username);
+    // This loop just finds the user with the username 
     while (platform != NULL)
     {
         // Find the username that you are looking for
         if (strcmp((*platform)->username, username) == 0)
         {
+            // If we found it, break out of the loop
             break;
         }
         // try the next user
@@ -180,33 +215,45 @@ void post_message(User **platform)
         }
         else
         {
+            // This is if the username doesn't exist
             printf("Error: We did not find anyone with that username\n");
             return;
         }
-        if ((*platform)->num_posts < MAX_POSTS)
-        {
-            printf("What would you like to post(It can't be more than 200 charecters)?\n");
-            scanf(" %200[^\n]", (*platform)->posts[(*platform)->num_posts]);
-            (*platform)->num_posts++;
-        }
-        else
-        {
-            printf("Error: You have already reached your maximum number of posts\n");
-            return;
-        }
+    }
+    // This if statement checks to make sure there arn't too many posts
+    if ((*platform)->num_posts < MAX_POSTS)
+    {
+        // Asks for what they want to post
+        printf("What would you like to post(It can't be more than 200 charecters)?\n");
+        scanf(" %200[^\n]", (*platform)->posts[(*platform)->num_posts]);
+        // increase the number of posts they have posted
+        (*platform)->num_posts++;
+    }
+    else
+    {
+        // This is incase they have already posted their limit
+        printf("Error: You have already reached your maximum number of posts\n");
+        return;
     }
 }
+// This function follows a user using the double pointer User platform struct
+// The time complexity of this function is O(n) because there is just one while loop
+// This function is void because we just follow a user and we don't have to use anything from this function, escpecially since it is a pointer
 void follow_user(User **platform)
 {
     // Implement following another user
+    // Declare the char
     char username[50];
+    // Ask for the username
     printf("What is your username\n");
     scanf(" %49[^\n]", username);
+    //  This loop searches for the username 
     while (platform != NULL)
     {
         // Find the username that you are looking for
         if (strcmp((*platform)->username, username) == 0)
         {
+            // Break when we find it
             break;
         }
         // try the next user
@@ -216,33 +263,45 @@ void follow_user(User **platform)
         }
         else
         {
+            // In case the username doesn't exist
             printf("Error: We did not find anyone with that username\n");
             return;
         }
     }
+    //This makes sure there anr't too many followers
     if ((*platform)->num_followers < MAX_FOLLOWERS)
     {
+        // Ask for who you want to follow and put it into the followers matrix
         printf("What username do you want to follow?");
         scanf(" %49[^\n]", (*platform)->username[(*platform)->num_followers]);
+        // Increase the number of followers you have
         (*platform)->num_followers++;
     }
+    // In case they have reached the maximum
     else
     {
         printf("Error: You have reached the maximum number of followers\n");
     }
 }
+// This function generates the feed using a pointer User users struct
+// The time complexity is O(n^s) because the nested loop dominates and is the biggest factor
+// This function is void because we just print out the posts of the followers and don't actually change anything
 void generate_feed(User *users)
 {
     // Implement generating a user's feed
+    // Declare the char and two ints
     char username[50];
     int i, j;
+    // Ask for the username
     printf("What is your username\n");
     scanf(" %49[^\n]", username);
+    // As long as the linked list isn't empty..≥
     while (users != NULL)
     {
         // Find the username that you are looking for
         if (strcmp(users->username, username) == 0)
         {
+            // Break when you have found it
             break;
         }
         // try the next user
@@ -252,30 +311,37 @@ void generate_feed(User *users)
         }
         else
         {
+            // In case the username dosn't exist
             printf("Error: We did not find anyone with that username\n");
             return;
         }
     }
+    // In case the user doesn;t have any followers
     if (users->followers == NULL)
     {
         printf("Error: You do not have any followers\n");
     }
     else
     {
+        // This loop will run from 0 to the number of followers
         for (i = 0; i < users->num_followers; i++)
         {
+            // Set the char temporaraly to to the i'th follower
             char follower = *users->followers[i];
+            // As long as the linked list isn't empty...
             while (users != NULL)
             {
                 // Find the username that you are looking for
                 if (strcmp(users->username, &follower) == 0)
                 {
+                    // Print the follower
                     printf("%s:\n", follower);
+                    // This loop goes from 0 to the number of posts the follower has
                     for (j = 0; j < users->num_posts; j++)
                     {
+                        // Print the j'th post
                         printf("%s\n", users->posts[j]);
                     }
-                    break;
                 }
                 // try the next user
                 if (users->next)
@@ -286,20 +352,28 @@ void generate_feed(User *users)
         }
     }
 }
+// This loads the information of a file into a linked list in the begining of main
+// This just asks for the pointer num_users
+// We return the linked list where we loaded the information from the file
+// The time complexity is O(n) because there is one while loop
 User *loadPlatform(int *num_users)
 {
+    // Open the file along with the structs we need
     FILE *platform;
     User head;
     head.next = NULL;
     User *previous = &head;
     User x;
+    // Open the file
     platform = fopen("users.bin", "rb");
     // If the file is empty, return NULL
     if (platform == NULL) {
         return NULL;
     }
+    // Read from the file once with size of User
     while (fread(&x, sizeof(User), 1, platform))
     {
+        // Print some information
         printf("username = %s email = %s\n", x.username, x.email);
         x.next = NULL;
         // Allocate space
@@ -310,21 +384,30 @@ User *loadPlatform(int *num_users)
         previous = previous->next;
         num_users++;
     }
+    // Close file and return the linked list
     fclose(platform);
     return head.next;
 }
+// This file saves the information from the linked list into the file at the end of main
+// The time complexity is O(n) because there is one loop
+// The functions needs the linked list and it is void since it is the last thing we do and it just puts info into the file
 void savePlatform(User *users)
 {
+    // Declare the file and open it
     FILE *platform;
+    // We want wb because it is a binary file and we want to get rid of whatever is in there since it will be outdated info
     platform = fopen("users.bin", "wb");
+    // As long as the linked list isn't empty...
     while (users != NULL)
     {
+        // Write the linked list into the file
         fwrite(users, sizeof(User), 1, platform);
         // save the next book
         users = users->next;
     }
 }
 /*
+These are the optional functions
 void unfollow_user(User *users, int num_users) {
 // Implement unfollowing a user
 }
